b5cf7d5b64ab443dd9ed8dceb4b649c1083eef0f
jmayta1984 <jmayta@pucp.edu.pe>
2025-11-06 14:50:14 -0500

    feat(favorites): implement favorite feature

---
+++ easy_travel/lib/features/favorites/presentation/blocs/favorite_list_bloc.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:easy_travel/features/favorites/presentation/blocs/favorite_list_event.dart';
+import 'package:easy_travel/features/favorites/presentation/blocs/favorite_list_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FavoriteListBloc extends Bloc<FavoriteListEvent, FavoriteListState> {
+  FavoriteListBloc() : super(FavoriteListState()) {
+    on<GetAllFavorites>(_getAllFavorites);
+
+    on<RemoveFavorite>(_removeFavorite);
+  }
+
+  FutureOr<void> _getAllFavorites(
+    GetAllFavorites event,
+    Emitter<FavoriteListState> emit,
+  ) {}
+
+  FutureOr<void> _removeFavorite(
+    RemoveFavorite event,
+    Emitter<FavoriteListState> emit,
+  ) {}
+}
