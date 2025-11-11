5768b649b980b97ef09aa945c1f9164d35be20a6
efrainbautista <efrain@thibau.io>
2025-11-11 15:37:12 -0500

    feat(destination): implement repository pattern

--- easy_travel/lib/features/home/presentation/blocs/home_bloc.dart
+++ easy_travel/lib/features/home/presentation/blocs/home_bloc.dart
@@ -1,14 +1,19 @@
 import 'dart:async';
 import 'package:easy_travel/core/enums/status.dart';
-import 'package:easy_travel/features/home/data/destination_service.dart';
+import 'package:easy_travel/features/favorites/domain/favorite_repository.dart';
 import 'package:easy_travel/features/home/domain/destination.dart';
+import 'package:easy_travel/features/home/domain/destination_repository.dart';
 import 'package:easy_travel/features/home/presentation/blocs/home_event.dart';
 import 'package:easy_travel/features/home/presentation/blocs/home_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  final DestinationService service;
-  HomeBloc({required this.service}) : super(HomeState()) {
+  final DestinationRepository destinationRepository;
+  final FavoriteRepository favoriteRepository;
+  HomeBloc({
+    required this.destinationRepository,
+    required this.favoriteRepository,
+  }) : super(HomeState()) {
     on<GetDestinationsByCategory>(_getDestinationsByCategory);
   }
 
@@ -26,9 +31,8 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     );
 
     try {
-      List<Destination> destinations = await service.getDestinations(
-        category: event.category.label,
-      );
+      List<Destination> destinations = await destinationRepository
+          .getDestinationsByCategory(event.category.label);
       emit(state.copyWith(status: Status.success, destinations: destinations));
     } catch (e) {
       emit(state.copyWith(status: Status.failure, message: e.toString()));
